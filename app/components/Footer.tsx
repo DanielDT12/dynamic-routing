44703c1fed62904c4ec5b74208e8f7dc4de9c062
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-09-30 14:33:48 +0200

    demo components

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+	return (
+		<footer className="grid place-content-center mt-8 p-8 bg-slate-900">
+			<p>Footer</p>
+		</footer>
+	);
+}
---
+++ app/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+	return (
+		<header className="flex gap-8 p-8 mb-8 bg-slate-900">
+			<p className="text-2xl">Dynamic Routing</p>
+		</header>
+	);
+}
