565ccda8a545d022b1d89eb506136147285316a6
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-10-01 14:50:28 +0200

    add link

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Header() {
 	return (
-		<header className="flex gap-8 p-8 mb-8 bg-slate-900">
+		<header className="flex justify-between p-8 mb-8 bg-slate-900 items-center">
 			<p className="text-2xl">Dynamic Routing</p>
+			<Link className="text-2xl font-bold" href={`/switches`}>
+				&larr; Go back
+			</Link>
 		</header>
 	);
 }
