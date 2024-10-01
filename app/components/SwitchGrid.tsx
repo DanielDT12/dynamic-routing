5bb4417d025534b2d5bf952b9ed50fc92cc57e55
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-10-01 13:28:41 +0200

    add div overlay on hover, switch grid

--- app/components/SwitchGrid.tsx
+++ app/components/SwitchGrid.tsx
@@ -4,12 +4,9 @@ import Image from "next/image";
 
 export default function SwitchGrid() {
 	return (
-		<ul className="grid grid-cols-3 grid-flow-dense">
+		<ul className="max-w-[70rem] m-auto grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-5 px-4">
 			{productData.map((product, index) => (
-				<li
-					className="border border-solid border-red-700"
-					key={product.id + index}
-				>
+				<li className="relative m-auto w-fit" key={product.id + index}>
 					<Link href={`/switches/${product.ref}`}>
 						<Image
 							src={product.image}
@@ -17,6 +14,9 @@ export default function SwitchGrid() {
 							width={200}
 							height={200}
 						/>
+						<div className="absolute inset-0 place-content-center text-center text-2xl font-bold opacity-0 hover:opacity-100 hover:bg-[rgba(0,0,0,0.75)]">
+							<p className="p-2">{product.name}</p>
+						</div>
 					</Link>
 				</li>
 			))}
