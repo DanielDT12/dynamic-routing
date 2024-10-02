128a2ee9e6674cfff34e7030b582e4422fae5c7f
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-10-02 10:17:40 +0200

    rm test text

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 export default function Home() {
 	return (
 		<div className="grid place-content-center min-h-screen">
-			<h1>test</h1>
 			<Link href={"/switches"}>Switches page</Link>
 		</div>
 	);
